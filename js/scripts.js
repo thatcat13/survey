c7e1e03ebf08af7e9cfc99c43676a3d00176c0bf
Cat Janowitz and Jenn Trass <cat+jenn@>
2017-10-18 17:52:16 -0700

    have three different input types but madlibs isn't working at the moment when I press submit

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,27 @@ $(document).ready(function() {
   $("#radio").submit(function(event) {
     var animal = $("input:radio[name=animal]:checked").val();
   });
+
+
+  $("form-control").submit(function(event) {
+    var beverage = $("#beverage").val();
+  });
+
+  $("form-group").submit(function(event) {
+    var nounInput1 = $("#noun1").val();
+    var verbInput = $("#verb").val();
+    var nounInput2 = $("#noun2").val();
+    var exclamationInput = $("#exclamation").val();
+
+    $(".noun1").text(nounInput1Input);
+    $(".noun2").text(nounInput2Input);
+    $(".exclamation").text(exclamationInputInput);
+    $(".verb").text(verbInputInput);
+    $(".noun1").text(nounInput1Input);
+
+    $("#story").show();
+
+    event.preventDefault();
+
+  });
 });
