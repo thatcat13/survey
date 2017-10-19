fbede78a345c8edb09916fd4ed2fc45aa9e06521
thatcat13 <thatcat13@gmail.com>
2017-10-19 12:53:10 -0700

    classmate john helps with syntax errors on .text inputs and taking out unnecessary variable in beverage form and calling it change not submit

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,30 @@
 $(document).ready(function() {
   $("#radio").submit(function(event) {
+    event.preventDefault();
     var animal = $("input:radio[name=animal]:checked").val();
   });
 
 
-  $("form-control").submit(function(event) {
-    var beverage = $("#beverage").val();
+  $("#beverage").change(function() {
+    // var beverage = $(this).val();
   });
 
-  $("form-group").submit(function(event) {
+  $("#madlibs").submit(function(event) {
+    event.preventDefault();
     var nounInput1 = $("#noun1").val();
     var verbInput = $("#verb").val();
     var nounInput2 = $("#noun2").val();
     var exclamationInput = $("#exclamation").val();
 
-    $(".noun1").text(nounInput1Input);
-    $(".noun2").text(nounInput2Input);
-    $(".exclamation").text(exclamationInputInput);
-    $(".verb").text(verbInputInput);
-    $(".noun1").text(nounInput1Input);
+    $(".noun1").text(nounInput1);
+    $(".noun2").text(nounInput2);
+    $(".exclamation").text(exclamationInput);
+    $(".verb").text(verbInput);
+    $(".noun1").text(nounInput1);
 
     $("#story").show();
 
-    event.preventDefault();
+
 
   });
 });
