fc4c3ce49f20f2eee3cc96fd596a5ec9706c9a96
Cat Janowitz <thatcat13@gmail.com>
2017-10-23 13:17:19 -0700

    refactor input form using a loop and .forEach

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,40 @@
+// $(document).ready(function() {
+//   $("#radio").submit(function(event) {
+//     event.preventDefault();
+//     var animal = $("input:radio[name=animal]:checked").val();
+//   });
+//
+//
+//   $("#beverage").change(function() {
+//     // var beverage = $(this).val();
+//   });
+
+  // $("#madlibs").submit(function(event) {
+  //   event.preventDefault();
+  //   var nounInput1 = $("#noun1").val();
+  //   var verbInput = $("#verb").val();
+  //   var nounInput2 = $("#noun2").val();
+  //   var exclamationInput = $("#exclamation").val();
+  //
+  //   $(".noun1").text(nounInput1);
+  //   $(".noun2").text(nounInput2);
+  //   $(".exclamation").text(exclamationInput);
+  //   $(".verb").text(verbInput);
+  //   $(".noun1").text(nounInput1);
+  //
+  //   $("#story").show();
+
 $(document).ready(function() {
-  $("#radio").submit(function(event) {
-    event.preventDefault();
-    var animal = $("input:radio[name=animal]:checked").val();
-  });
-
-
-  $("#beverage").change(function() {
-    // var beverage = $(this).val();
-  });
-
   $("#madlibs").submit(function(event) {
-    event.preventDefault();
-    var nounInput1 = $("#noun1").val();
-    var verbInput = $("#verb").val();
-    var nounInput2 = $("#noun2").val();
-    var exclamationInput = $("#exclamation").val();
+  var blanks = ["noun1", "verb", "noun2", "exclamation"];
 
-    $(".noun1").text(nounInput1);
-    $(".noun2").text(nounInput2);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun1").text(nounInput1);
+  blanks.forEach(function(blank) {
+    var userInput = $("input#" + blank).val();
+    $("." + blank).text(userInput);
+  });
 
-    $("#story").show();
+  $("#story").show();
 
-
-
+  event.preventDefault();
   });
 });
